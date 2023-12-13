c8dd2c14fcabe7ee374780d8ef50397bc236f8a3
Steijn <433269@student.fontys.nl>
2023-12-13 12:07:20 +0100

    timer stop

--- FreshHeadBackend/Logic/TimerService.cs
+++ FreshHeadBackend/Logic/TimerService.cs
@@ -17,7 +17,11 @@ namespace FreshHeadBackend.Logic
             return Task.CompletedTask;
         }
 
-
+        public Task StopAsync(){
+            Console.WriteLine("Timed hosted service stopped");
+            timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
         public void StatusUpdate(object state)
         {
             Console.WriteLine("StatusUpdate sent");
